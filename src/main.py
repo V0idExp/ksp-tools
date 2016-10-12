fa3fa634b5539a7c3695c8bbbb2923353f24e01b
Ivan Nikolaev <voidexp@gmail.com>
2016-10-12 15:53:06 +0200

    Enhanced `stage` tool

--- src/main.py
+++ src/main.py
@@ -25,6 +25,10 @@ def _dv(empty, full, isp):
     return isp * KSP_G0 * log(full / empty)
 
 
+def _twr(body, mass, thrust):
+    return sum(thrust) / (mass * KSP_BODY_G_CONSTANTS[body])
+
+
 @click.command()
 @click.argument('empty', type=click.FLOAT)
 @click.argument('full', type=click.FLOAT)
@@ -40,28 +44,30 @@ def dv(empty, full, isp):
 @click.argument('thrust', nargs=-1, type=click.FLOAT)
 def twr(body, mass, thrust):
     """Compute stage TWR."""
-    result = sum(thrust) / (mass * KSP_BODY_G_CONSTANTS[body])
-    print('TWR = {}'.format(round(result, 2)))
+    print('TWR = {}'.format(round(_twr(body, mass, thrust), 2)))
 
 
 @click.command()
 @click.argument('payload', type=click.FLOAT)
 @click.argument('eng_mass', type=click.FLOAT)
+@click.argument('eng_thrust', type=click.FLOAT)
 @click.argument('eng_isp', type=click.FLOAT)
-@click.argument('tank_d', type=click.FLOAT)
-@click.argument('tank_f', type=click.FLOAT)
+@click.argument('tank_dry', type=click.FLOAT)
+@click.argument('tank_full', type=click.FLOAT)
 @click.argument('n_tanks', type=click.FLOAT)
 @click.option('--stacks', type=click.INT, default=1)
-def stage(payload, eng_mass, eng_isp, tank_d, tank_f, n_tanks, stacks):
+@click.option('--body', type=click.Choice(KSP_BODY_G_CONSTANTS.keys()), default='kerbin')
+def stage(payload, eng_mass, eng_thrust, eng_isp, tank_dry, tank_full, n_tanks, stacks, body):
     """Compute stage info for given configuration."""
-    stack_dry = eng_mass + tank_d * n_tanks
-    stack_full = eng_mass + tank_f * n_tanks
+    stack_dry = eng_mass + tank_dry * n_tanks
+    stack_full = eng_mass + tank_full * n_tanks
     mass_dry = payload + stack_dry * stacks
     mass_full = payload + stack_full * stacks
 
     print('Dry mass:   {} t'.format(mass_dry))
     print('Total mass: {} t'.format(mass_full))
     print('Δv:         {} m/s'.format(round(_dv(mass_dry, mass_full, eng_isp), 2)))
+    print('TWR:        {} ({})'.format(round(_twr(body, mass_full, [eng_thrust]), 2), body))
 
 
 cli.add_command(dv)
