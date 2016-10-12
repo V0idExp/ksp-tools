a437b20c83a31ae30c5e1d95514d09104f6f0cfe
Ivan Nikolaev <voidexp@gmail.com>
2016-10-12 18:50:13 +0200

    Add orbital_spd tool

--- src/main.py
+++ src/main.py
@@ -3,16 +3,25 @@
 """Main CLI application."""
 
 from math import log
+from math import sqrt
 import click
 
 #: Kerbal g₀ value
 KSP_G0 = 9.82
 
 
-#: Kerbal bodies local gravity acceleration constants
-KSP_BODY_G_CONSTANTS = {
-    'kerbin': 9.82,
-    'mun': 1.66,
+#: Kerbal bodies information
+KSP_BODIES = {
+    'kerbin': {
+        'g0': 9.82,
+        'std_grav_param': 3.5316423e12,
+        'radius': 600000,
+    },
+    'mun': {
+        'g0': 1.66,
+        'radius': 200000,
+        'std_grav_param': 6.5139178e10,
+    },
 }
 
 
@@ -26,7 +35,11 @@ def _dv(empty, full, isp):
 
 
 def _twr(body, mass, thrust):
-    return sum(thrust) / (mass * KSP_BODY_G_CONSTANTS[body])
+    return sum(thrust) / (mass * KSP_BODIES[body]['g0'])
+
+
+def _orbital_spd(body, height):
+    return sqrt(KSP_BODIES[body]['std_grav_param'] / (KSP_BODIES[body]['radius'] + height))
 
 
 @click.command()
@@ -39,12 +52,20 @@ def dv(empty, full, isp):
 
 
 @click.command()
-@click.argument('body', type=click.Choice(KSP_BODY_G_CONSTANTS.keys()))
+@click.argument('body', type=click.Choice(KSP_BODIES.keys()))
 @click.argument('mass', type=click.FLOAT)
 @click.argument('thrust', nargs=-1, type=click.FLOAT)
 def twr(body, mass, thrust):
     """Compute stage TWR."""
     print('TWR = {}'.format(round(_twr(body, mass, thrust), 2)))
+
+
+@click.command()
+@click.argument('body', type=click.Choice(KSP_BODIES.keys()))
+@click.argument('height', type=click.FLOAT)
+def orbital_spd(body, height):
+    """Compute speed for given orbit around a body."""
+    print('Speed = {}'.format(round(_orbital_spd(body, height), 2)))
 
 
 @click.command()
@@ -56,7 +77,7 @@ def twr(body, mass, thrust):
 @click.argument('tank_full', type=click.FLOAT)
 @click.argument('n_tanks', type=click.FLOAT)
 @click.option('--stacks', type=click.INT, default=1)
-@click.option('--body', type=click.Choice(KSP_BODY_G_CONSTANTS.keys()), default='kerbin')
+@click.option('--body', type=click.Choice(KSP_BODIES.keys()), default='kerbin')
 def stage(payload, eng_mass, eng_thrust, eng_isp, tank_dry, tank_full, n_tanks, stacks, body):
     """Compute stage info for given configuration."""
     stack_dry = eng_mass + tank_dry * n_tanks
@@ -73,6 +94,7 @@ def stage(payload, eng_mass, eng_thrust, eng_isp, tank_dry, tank_full, n_tanks, 
 cli.add_command(dv)
 cli.add_command(twr)
 cli.add_command(stage)
+cli.add_command(orbital_spd)
 
 
 if __name__ == '__main__':
